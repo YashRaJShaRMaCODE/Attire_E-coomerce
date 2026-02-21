8353b83f32818744e6cb75325863f938c8a0d438
yash <yashrajsharma786jmd@gmail.com>
2026-02-22 01:07:30 +0530

    initial commit

---
+++ home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,HttpResponse
+from home.models import Contact
+
+# Create your views here.
+def index(request):
+    context={'variable':"this is sent"}
+    return render(request,'index.html',context)
+def about(request):
+    return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+
+    return render(request, 'contact.html')
+def feedback(request):
+    return render(request,'feedback.html')
